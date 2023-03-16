4fad9cc411377063fc105f1a02ccec1520c6ee46
Guilherme Ferreira <guilherme.d.ferreira@cgi.com>
2023-03-16 11:37:39 +0000

    Add Tailwind

--- Cgi.Appmar.Web/app/src/index.js
+++ Cgi.Appmar.Web/app/src/index.js
@@ -4,7 +4,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import './index.scss';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './i18n/config';
