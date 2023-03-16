626aaf0643bd24c53730af543dce301953d3d9dc
Guilherme Ferreira <guilherme.d.ferreira@cgi.com>
2023-03-16 10:26:20 +0000

    ADD Translations

--- Cgi.Appmar.Web/app/src/App.js
+++ Cgi.Appmar.Web/app/src/App.js
@@ -1,10 +1,14 @@
 import Layout from './Layout';
 import './App.scss';
+import {useTranslation} from 'react-i18next';
 
 function App() {
+
+  const { t } = useTranslation();
+
   return (
     <div>
-      
+      <h1>{t('helloWorld')}</h1>
       <Layout />
       
     </div>
